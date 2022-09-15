2be15e59e1601a42f2ee1657dbf845aa5b0b9231
Heinrikii <henrikii@outlook.com>
2022-09-15 15:29:30 -0300

    Meu Projeto Conta Banco

---
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //TODO:Conhecer e importar a Classe Scanner
+        Scanner input = new Scanner(System.in);
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+
+        //Exibir a mensagem conta criada
+
+        input.close();
+    }
+}
